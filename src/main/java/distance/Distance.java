fc81e6bd0489425812a1a6034b78cc365c53fd7e
dcnorris <dcnorris@uncc.edu>
2015-07-14 11:21:13 -0400

    mavenized project

--- src/main/java/distance/Distance.java
+++ src/main/java/distance/Distance.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package distance;
 
@@ -8,7 +8,7 @@ package distance;
  *
  */
 public interface Distance {
-	
-	public int getDistance(Object object1, Object object2);
+
+    public int getDistance(Object object1, Object object2);
 
 }
--- src/main/java/distance/LevenshteinDistance.java
+++ src/main/java/distance/LevenshteinDistance.java
@@ -1,17 +1,17 @@
 package distance;
 
 /**
- * http://en.wikipedia.org/wiki/Levenshtein_distance 
+ * http://en.wikipedia.org/wiki/Levenshtein_distance
  */
 public class LevenshteinDistance implements Distance {
 
-	@Override
-	public int getDistance(Object object1, Object object2) {
-		
-		String string1 = (String) object1;
-		String string2 = (String) object2;
-		
-		int distance[][]; // distance matrix
+    @Override
+    public int getDistance(Object object1, Object object2) {
+
+        String string1 = (String) object1;
+        String string2 = (String) object2;
+
+        int distance[][]; // distance matrix
         int n; // length of first string
         int m; // length of second string
         int i; // iterates through first string
@@ -23,44 +23,47 @@ public class LevenshteinDistance implements Distance {
         // Step 1
         n = string1.length();
         m = string2.length();
-        if (n == 0)
+        if (n == 0) {
             return m;
-        if (m == 0)
+        }
+        if (m == 0) {
             return n;
-        distance = new int[n+1][m+1];
+        }
+        distance = new int[n + 1][m + 1];
 
         // Step 2
-        for (i = 0; i <= n; i++)
+        for (i = 0; i <= n; i++) {
             distance[i][0] = i;
-        for (j = 0; j <= m; j++)
+        }
+        for (j = 0; j <= m; j++) {
             distance[0][j] = j;
+        }
 
         // Step 3
-        for (i = 1; i <= n; i++)
-        {
-            s_i = string1.charAt (i - 1);
+        for (i = 1; i <= n; i++) {
+            s_i = string1.charAt(i - 1);
 
             // Step 4
-            for (j = 1; j <= m; j++)
-            {
+            for (j = 1; j <= m; j++) {
                 t_j = string2.charAt(j - 1);
 
                 // Step 5
-                if (s_i == t_j)
+                if (s_i == t_j) {
                     cost = 0;
-                else
+                } else {
                     cost = 1;
+                }
 
                 // Step 6
-                distance[i][j] = findMinimum(distance[i-1][j]+1, distance[i][j-1]+1, distance[i-1][j-1] + cost);
+                distance[i][j] = findMinimum(distance[i - 1][j] + 1, distance[i][j - 1] + 1, distance[i - 1][j - 1] + cost);
             }
         }
 
         // Step 7
         return distance[n][m];
-	}
-	
-	private int findMinimum(int a, int b, int c) {
+    }
+
+    private int findMinimum(int a, int b, int c) {
         int min = a;
         if (b < min) {
             min = b;
